a58526bd64283416d08e4ac6e843c21979b8c30d
Maarij Bukhari <syedmuhammadmaarij@gmail.com>
2024-09-12 23:31:33 +0500

    Initiail commit

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,34 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public nftAddress;
+    address payable public seller; // It's payable because he is going to receive crypto currency
+    address public lender;
+    address public inspector;
 
+    modifier onlySeller() {
+        require(msg.sender==seller,"Only Seller can call this method");
+        _;
+    }
+
+    mapping(uint256=>bool) public isListed;
+    mapping(uint256=>uint256) public purchasePrice;
+    mapping(uint256=>uint256) public escrowAmount;
+    mapping(uint256=>address) public buyer;
+
+    constructor(address _nftAddress, address payable _seller, address _inspector , address _lender){
+        nftAddress = _nftAddress;
+        seller = _seller;
+        lender= _lender;
+        inspector = _inspector;
+    }
+
+    function list(uint256 _nftID,address _buyer ,uint256 _purchasePrice,uint256 _escrowAmount) public payable onlySeller {
+        //Transfer NFT from seller to this contact
+        IERC721(nftAddress).transferFrom(msg.sender,address(this),_nftID);
+        isListed[_nftID]=true;
+        purchasePrice[_nftID]=_purchasePrice;
+        escrowAmount[_nftID]=_escrowAmount;
+        buyer[_nftID]=_buyer;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,75 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender;
+    let realEstate, escrow;
 
+    beforeEach(async () => {
+        //getting Signers just setting up dummy accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+        //Deploy Real Estate
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+
+        //Mint
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json");
+        await transaction.wait();
+
+        const Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await Escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address);
+
+        //Approve Property
+        transaction=await realEstate.connect(seller).approve(escrow.address,1)
+        await transaction.wait();
+
+        //List Property
+        transaction=await escrow.connect(seller).list(1,buyer.address,tokens(10),tokens(5));
+        await transaction.wait();
+    })
+
+    describe('Deployment', () => {
+        it('Returns NFT address', async () => {
+            const result = await escrow.nftAddress();
+            expect(result).to.be.equal(realEstate.address);
+        })
+        it('Returns seller', async () => {
+            const result = await escrow.seller();
+            expect(result).to.be.equal(seller.address);
+        })
+        it('Returns lender', async () => {
+            const result = await escrow.lender();
+            expect(result).to.be.equal(lender.address);
+        })
+        it('Returns inspector', async () => {
+            const result = await escrow.inspector();
+            expect(result).to.be.equal(inspector.address);
+        })
+    })
+    describe('Listing', () => {
+
+        it('Updates As Listed',async()=>{
+            const result=await escrow.isListed(1);
+            expect(result).to.be.equal(true);
+        })
+
+        it('Changes OwnerShip', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
+        })
+
+        it('Returns buyer',async()=>{
+            const result=await escrow.buyer(1);
+            expect(result).to.be.equal(buyer.address);
+        })
+        it('Returns purchase Price',async()=>{
+            const result=await escrow.purchasePrice(1);
+            expect(result).to.be.equal(tokens(10));
+        })
+        it('Returns escrow Amount',async()=>{
+            const result=await escrow.escrowAmount(1);
+            expect(result).to.be.equal(tokens(5));
+        })
+
+        
+
+    })
 })
